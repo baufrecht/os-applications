52534609bc923501b2a2ba36c57c247453724d84
Steffen Hoenig <s.hoenig@openthinclient.com>
2014-04-15 12:55:47 +0200

    Removed tcos-branding, since it's deprecated.

--- tcos-branding/tcos-branding-2.0/tcos/PostLogin/tcos-branding.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python 
-
-import os
-
-# update glib schema
-#os.system('sudo glib-compile-schemas /usr/share/glib-2.0/schemas/')
-# update dconf-suttings
-os.system('sudo dconf update')
