088c672ac23dca1e464f95f86a94869137f0d2f5
Steffen Hoenig <mail@steffenhoenig.com>
2014-09-03 18:08:11 +0200

    refresh package-set

--- ibm/tn5250j/tn5250lpr/src/org/tn5250jlpr/event/BootListener.java
+++
@@ -1,9 +0,0 @@
-package org.tn5250jlpr.event;
-
-import java.util.EventListener;
-
-public interface BootListener extends EventListener {
-
-    public abstract void bootOptionsReceived(BootEvent bootevent);
-
-}
